810f55ad4acf9bcb7b2be587fc44219c4ffaf567
Leonardo Albuquerque <leeodesign@hotmail.com>
2022-09-11 16:05:31 -0300

    feat: create interfaces and services

---
+++ src/app/interfaces/Comment.ts
@@ -0,0 +1,8 @@
+export interface Comment {
+  id?: number
+  comment: string
+  username: string
+  momentId: number,
+  created_at?: Date 
+  updated_at?: Date 
+}
---
+++ src/app/interfaces/Moment.ts
@@ -0,0 +1,9 @@
+interface Moment {
+  id?: number
+  title: string
+  description: string
+  image: string
+  created_at: Date
+  update_at: Date
+  comments?: [{comment: string, username: string}]
+}
